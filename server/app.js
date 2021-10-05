70ebc0e0538e369562c15ef6ab45c37df330a8a1
Oskar Strömberg <oskstr@kth.se>
2021-10-06 00:55:57 +0200

    Fetch all specification files automatically

--- server/app.js
+++ server/app.js
@@ -16,25 +16,19 @@ app.use(express.json())
 if (configuration.NODE_ENV === "development") app.use(morgan('dev'))
 else app.use(morgan("common"))
 
-const duckumentation = fs.readFileSync(path.resolve(__dirname + "/specifications/duckumentation.yml"))
-app.get("/api/duckumentation", (req, res) => res.send(duckumentation.toString()))
+const configure = (specification) => {
+    const { name } = path.parse(specification)
+    const system = fs.readFileSync(path.resolve(`${__dirname}/specifications/${specification}`));
+    app.get(`/api/${name}`, (req, res) => res.send(system.toString()));
+}
 
-const nallen = fs.readFileSync(path.resolve(__dirname + "/specifications/nallen.yml"))
-app.get("/api/nallen", (req, res) => res.send(nallen.toString()))
+const specifications = fs.readdirSync(`${__dirname}/specifications/`)
+specifications.map(system => configure(system));
 
-const damm = fs.readFileSync(path.resolve(__dirname + "/specifications/damm.yml"))
-app.get("/api/damm", (req, res) => res.send(damm.toString()))
-
-const dfunkt = fs.readFileSync(path.resolve(__dirname + "/specifications/dfunkt.yml"))
-app.get("/api/dfunkt", (req, res) => res.send(dfunkt.toString()))
-
-const spam2 = fs.readFileSync(path.resolve(__dirname + "/specifications/spam2.yml"))
-app.get("/api/spam2", (req, res) => res.send(spam2.toString()))
-
-// Insert new API endpoints above this line
+// Insert new API endpoints by adding more files to /specifications
 app.get("/api/*", (req, res) => res.send("Invalid API path"))
 
-app.use("/", express.static(path.join(__dirname + '/../client/build')))
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + "/../client/build/index.html")))
+app.use("/", express.static(path.join(`${__dirname}/../client/build`)))
+app.get('*', (req, res) => res.sendFile(path.resolve(`${__dirname}/../client/build/index.html`)))
 const PORT = configuration.PORT
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
